a2dc5d57d8eb6d9c229294a7dd74d2691f2be555
Thomas Plunkett <thomas.plunkett@chrec.org>
2024-03-19 15:34:31 -0400

    update

--- log_splitter.py
+++ log_splitter.py
@@ -1,6 +1,9 @@
 # Helper script to paste a formatted log string into separate files
 
-upd = '''[PASTE_NEW_LOGS]'''
+upd = '''[2024-03-05]: Completed the Chattanooga burst downlink and began another!  One target programmed!
+[2024-03-08]: Completed manual downlink of last few segments of coastal burst near Tokyo!  Two more targets programmed!
+[2024-03-12]: Poor downlink window today.  Conducting manual imaging and storing on SGPU for use on future ML/CV apps after the move!
+[2024-03-13]: Race track near Melbourne, Australia visible in one of the 3/8 bursts!  Began downlink of burst near Plymouth, Massachusetts!  Two more bursts scheduled!'''
 
 for line in upd.split('\n'):
     date = line.split(']')[0].split('[')[1]
