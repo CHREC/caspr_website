cbb32fe7e8297d5fb6b220ea5275f6aa0f1aa14e
Thomas Plunkett <thomas.plunkett@chrec.org>
2024-04-01 15:06:18 -0400

    Week of 4/1 log update

--- log_splitter.py
+++ log_splitter.py
@@ -1,9 +1,10 @@
 # Helper script to paste a formatted log string into separate files
 
-upd = '''[2024-03-05]: Completed the Chattanooga burst downlink and began another!  One target programmed!
-[2024-03-08]: Completed manual downlink of last few segments of coastal burst near Tokyo!  Two more targets programmed!
-[2024-03-12]: Poor downlink window today.  Conducting manual imaging and storing on SGPU for use on future ML/CV apps after the move!
-[2024-03-13]: Race track near Melbourne, Australia visible in one of the 3/8 bursts!  Began downlink of burst near Plymouth, Massachusetts!  Two more bursts scheduled!'''
+upd = '''[2024-03-20]: Manually completed last few segments of Geelong and Plymouth bursts!  Started downlink of Constitucion burst.  Two more burst targets programmed!
+[2024-03-22]: Downlinked new round of radiation-effects data!  Beginning downlink of San Salvador burst!  Two more bursts scheduled!
+[2024-03-26]: Down to only four segments remaining for San Salvador burst!  Started downlink of burst near Sanxenxo, Spain!  12 bursts stored onboard!
+[2024-03-27]: Lots of burst capture data movement.  Began downlink of Melbourne burst!  Two more burst targets this evening!
+[2024-03-29]: Back-to-back manual burst successes near San Juan, Puerto Rico and Cape Canaveral, Florida!  Lots of data operations!  Additional burst attempt scheduled for this evening!'''
 
 for line in upd.split('\n'):
     date = line.split(']')[0].split('[')[1]
