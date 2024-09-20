a4e3d04eca8953317b103dd21cb42ac05180315f
Thomas Plunkett <thomas.plunkett@nsf-shrec.org>
2024-09-20 14:02:37 -0400

    test

--- log_splitter.py
+++ log_splitter.py
@@ -1,15 +1,7 @@
 # Helper script to paste a formatted log string into separate files
 
-upd = '''[2024-05-24]: Downlinking single-frame images stored in memory from previous CASPR location!  Began downlink of Santo Domingo burst!
-[2024-05-31]: Began downlink of San Juan burst!  Tested image conversion commands for coming app upload.
-[2024-06-06]: Successfully uploaded CNNJPEG image compression app for on-orbit study!  Shout out to Diego and Tyler for their hard work!
-[2024-06-14]: Began downlink of Merritt Island burst!  Collected another round of radiation-effects data.
-[2024-06-21]: Completed downlink of Santo Domingo burst!  Unique cityscape with multiple airports!
-[2024-06-28]: Completed downlink of San Juan burst!  Unexpected parts of denser urban area visible further down the frame!
-[2024-07-19]: Downlinking second half of Merritt Island burst!
-[2024-07-26]: Acquired last few lost segments of first half of Merritt Island burst!  Halfway there!
-[2024-08-02]: Debugging a slight anomaly with second half of Merritt Island burst.  This one is worth the fight, though!
-[2024-08-09]: Completed downlink of Merritt Island burst!  Began downlink of Palermo, Italy burst!'''
+upd = '''[2024 09 17]: Successfully downlinked a stored partial burst from near San Antonio, Texas!  Unfortunately, this burst is mainly cloud cover.
+[2024 09 20]: Successfully downlinked remaining half of San Juan burst!  Compressed and began downlink of cloudy Pittsburgh burst!'''
 
 for line in upd.split('\n'):
     date = line.split(']')[0].split('[')[1]
