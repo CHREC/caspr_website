41c3c38ca899e85cced11df4aa232b0fd082545e
Thomas Plunkett <thomas.plunkett@nsf-shrec.org>
2024-12-12 13:21:54 -0500

    last update of the semester

--- log_splitter.py
+++ log_splitter.py
@@ -1,11 +1,6 @@
 # Helper script to paste a formatted log string into separate files
 
-upd = '''[2024-10-31]: Demonstrated CASPR operations to visitors from Sandia National Laboratories!
-[2024-11-01]: Successfully split compressed Palermo burst on orbit, recompressed in smaller halves, and began downlink!
-[2024-11-05]: Began downlink of NIR/R half of Palermo burst!
-[2024-11-07]: Demonstrated CASPR operations to visitor from Lockheed Martin!  A few manual transfers of Palermo burst segments.
-[2024-11-08]: Completed, recombined, and decompressed Palermo burst on the ground!  Cleared space on GPU system for further experiments, including CNNJPEG!
-[2024-11-14]: Successfully decompressed, split, and recompressed New Guinea burst onboard and began downlink!'''
+upd = '''[2024-12-10]: Downlinked another round of radiation-effects data from CSP and SSPs!  Preparing for on-orbit CNNJPEG tests, but this has been delayed slightly due to high beta angle and resultant high temperatures.'''
 
 for line in upd.split('\n'):
     date = line.split(']')[0].split('[')[1]
