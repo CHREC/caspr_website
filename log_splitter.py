8c483d627a6c6ad56e19bae519ff17492178addb
Thomas Plunkett <thomas.plunkett@nsf-shrec.org>
2025-04-22 17:12:49 -0400

    Earth Day UpDaTe!

--- log_splitter.py
+++ log_splitter.py
@@ -1,6 +1,12 @@
 # Helper script to paste a formatted log string into separate files
 
-upd = '''[2024-12-10]: Downlinked another round of radiation-effects data from CSP and SSPs!  Preparing for on-orbit CNNJPEG tests, but this has been delayed slightly due to high beta angle and resultant high temperatures.'''
+upd = '''[2025-03-28]: Working to resolve an issue with SSP1 and grainy image burst capture.  Also performed some diagnostics on SGPU swap space for CNNJPEG!
+[2025-03-31]: Conducted extensive flatsat testing to help resolve a CNNJPEG bug dealing with limited storage memory and swap size.
+[2025-04-03]: Demonstrated CASPR operations to SHREC Members from Northrup Grumman!  Thank you very much for your visit and support!
+[2025-04-07]: Members of the CASPR team attended the National Conference on Undergraduate Research in Pittsburgh to help recruit the next incoming class of Pitt Space graduate engineers!
+[2025-04-08]: Showcased CASPR commanding to Pitt ECE Distinguished Speaker Dr. Don Tan!  Thank you very much for sharing your time and knowledge with SHREC!
+[2025-04-10]: Provided CASPR data and experiment updates to Pitt Space Distinguished Speaker Dr. Andrew Horchler!  Thank you very much for your visit and insight!
+[2025-04-18]: Resolved limited storage issue for CNNJPEG processing!  Successfully completed first three layers of compression!'''
 
 for line in upd.split('\n'):
     date = line.split(']')[0].split('[')[1]
